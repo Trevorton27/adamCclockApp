3b977fcd0953c92768d29333f52eeac1212e0de0
Trevor Mearns <spiral272@gmail.com>
2021-06-11 12:01:48 +0900

    ready for code review approval

--- date_and_time.js
+++ date_and_time.js
@@ -1,34 +1,83 @@
-function date_and_time() {
-    var date = new Date();
-    var second = date.getSeconds();
-    var minuet = date.getMinutes();
-    var hour = date.getHours();
-    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var day_of_week = days[date.getDay()];
-    var month = months[date.getMonth()];
-    var day = date.getDate();
-    var year = date.getFullYear();
-  
-  
-    day = (day < 10) ? "0" + day : day
-  
-    var date = day_of_week + ", " + month + " " + day + ", " + year;
-    document.getElementById("date").innerText = date;
-  
-  
-    var suffix = "AM";
-  
-    if (hour == 0) {hour = 12;}
-    if (hour > 12) {hour = hour - 12; suffix = "PM";}
-  
-    hour = (hour < 10) ? "0" + hour : hour;
-    minuet = (minuet < 10) ? "0" + minuet : minuet;
-    second = (second < 10) ? "0" + second : second;
-    
-    var time = hour + ":" + minuet + ":" + second + " " + suffix;
-    document.getElementById("time").innerText = time;
-    
+function renderTime() {
+  const theTime = new Date();
+  const second = addLeadingZero(theTime.getSeconds());
+  const minute = addLeadingZero(theTime.getMinutes());
+  const hour = theTime.getHours();
+  const isAm = hour < 12 || hour === 0;
+  let amPm = isAm ? 'AM' : 'PM';
+
+  const displayTime = `${formatHour(hour)}:${minute}:${second} ${amPm}`;
+  document.getElementById('time').innerText = displayTime;
+}
+
+function addLeadingZero(number) {
+  return number < 10 ? '0' + number : number;
+}
+
+function formatHour(hour) {
+  hour = hour >= 13 ? hour - 12 : hour;
+
+  hour = hour === 0 ? hour + 12 : hour;
+  return hour;
+}
+
+function renderDate() {
+  const theDate = new Date();
+
+  const day_of_week = days[theDate.getDay()];
+  const month = months[theDate.getMonth()];
+  const dateToday = theDate.getDate();
+  const year = theDate.getFullYear();
+
+  const date = `${day_of_week}, ${month} ${formatDateSuffix(
+    dateToday
+  )} ${year}`;
+  document.getElementById('date').innerText = date;
+}
+
+function formatDateSuffix(date) {
+  if (date < 10 || date > 20) {
+    switch (date % 10) {
+      case 1:
+        return `${date}st`;
+      case 2:
+        return `${date}nd`;
+      case 3:
+        return `${date}rd`;
+    }
   }
-  setInterval(date_and_time, 1000);
-  date_and_time();
+  return `${date}th`;
+}
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+
+renderTime();
+renderDate();
+
+setInterval(() => {
+  renderTime();
+  renderDate();
+}, 1000);
